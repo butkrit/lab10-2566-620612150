5c449aaa6fe50eab070700d257501f25b6d5c03a
butkrit <yokart.butter@gmail.com>
2023-07-26 14:33:23 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,36 @@
 "use client";
-
-import axios from "axios";
-import { useState } from "react";
+import { cleanUser } from "@/libs/cleanUser";
+import axios from "axios";	
+import { useEffect, useState } from "react";
+import { UserCard } from "@/components/UserCard";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+    useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+      return;
+    }
+    const strAmount = JSON.stringify(genAmount);
+    localStorage.setItem("genAmount", strAmount);
+    }, [genAmount]);
+
+  useEffect(() => {
+    const strAmount = localStorage.getItem("genAmount");
+    if (strAmount == null) {
+      setGenAmount(1);
+      return;
+    }
+    const loadedAmount = JSON.parse(strAmount);
+    setGenAmount(loadedAmount);
+  }, []);
+
+
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -19,6 +42,8 @@ export default function RandomUserPage() {
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
+     const cleanedUser = users.map((a) => cleanUser(a));
+     setUsers(cleanedUser);
   };
 
   return (
@@ -40,7 +65,15 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users && !isLoading && users.map((a) => (
+          <UserCard
+            key={a.email}
+            name={a.name}
+            imgUrl={a.imgUrl}
+            address={a.address}
+            email={a.email}
+          />
+        ))}
     </div>
   );
 }
